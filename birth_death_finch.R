d965714376bb23467a4d33c3368ce29f65e11776
pupupanda <39288373+pupupanda@users.noreply.github.com>
2018-06-15 02:26:23 +1200

    Add files via upload

--- birth_death_finch.R
+++ birth_death_finch.R
@@ -1,13 +1,16 @@
 library(ape)
 library(BioGeoBEARS)
 
-#R code for a birth-death model
+
+#R code for the ML for a birth-death model
 #This model is a model which allows species to speciate with a rate lamba, as well as to die with a rate mu
 #In this model, clades are expected growing exponentially at the net diversification rate.
-
+wd <- ("D:/2018 semster 1/Bioinf 702/assignment 4")
+setwd(wd)
 
 #load tree data into R
 tree <- read.nexus("http://www.r-phylo.org/w/images/0/02/Geospiza.nex")
+#the birth-death function from ape package, we can compare the output
 birthdeath(tree)
 
 
@@ -32,15 +35,59 @@ log_likelihood <- function(lambda, mu, phy) {
   N = length(tree$tip.label)
   #branching time of each species
   x <- c(NA, branching.times(tree))
- 
+ plot(tree)
  #Set up the condition for the function 
- #If the speciation rate is smaller than 0 or the extinction rate is larger than 1, then return to a very small number   
+  #If the speciation rate is smaller than 0 or the extinction rate is larger than 1
+  #then there won't be a tip to be observed. 
+  #return the log likelehood to a very small number   
     
     if (r  < 0 || a  > 1){
       Lnl = exp(0.001)
     }   else{
- #If the speciation rate is larger than 0 or the extinction rate is smaller than 1, then do the log likelihood calculation
- #
+  #If the speciation rate is larger than 0 or the extinction rate is smaller than 1, then do the log likelihood calculation
+  
+      ##The log_likelihood calculation break down
+      
+      # There are (n-1)! possible topologies for any set of n-1 waiting times
+      # Log (factorial(numtips-1))
+      
+      # the number of internal nodes is the number of tips -2 (n-2)
+      # the expected waiting time is net diversification rate 
+      # log expected waiting time  for internal branches
+      # (N - 2) * log(r) 
+       
+      
+      
+      # total length  of internal nodes not including root: sum(x[3:N])
+      # total amount of diversification on these 2 branches
+      # r * sum(x[3:N]) 
+      
+      # N is the number of tip which means there is no speciation nor extinction.
+      # log(Waiting time of nothing happen)
+      # N * log(1 - a) 
+      
+      # there are two branches associatied with each node
+      # - 2 * sum(log(exp(r * x[2:N]) - a))
+  
+      # ages of each internal branches
+      # x[2:N]
+      
+    
+      # net diversification from each node
+      # r * x[2:N]
+      
+      # expected amount of diversification from each node
+      # exp(r * x[2:N])
+      
+      # expected amount of diversification from each node, 
+      # minus number of lineages expected to go extinct from each node
+      # exp(r * x[2:N]) - a
+      
+      
+      # amount of waiting time from each internal node
+      # sum(log(exp(r * x[2:N]) - a))
+      
+      
   
       
       Lnl = lfactorial(N - 1) + (N - 2) * log(r) + r * sum(x[3:N]) + N * log(1 - a) - 2 * sum(log(exp(r * x[2:N]) - a))
@@ -57,7 +104,7 @@ print(paste("lambda=",lambda,"mu=", mu, "Lnl=",Lnl))
 }
 
 #optimise the birth and death rate
-#Find value of lambda and mu by maximizing the log_likelihood given the tree.
+#Find the maximum value of lambda and mu by using optim on  the log_likelihood function
 optim(c(lambda,mu), fn = function(p)log_likelihood(lambda = p[1],mu=p[2],phy = tree), method=c("L-BFGS-B"),lower = 0, control = list(fnscale = -1))
 
 
